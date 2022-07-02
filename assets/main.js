f632bbdb69f8f5efcd21525c5427cccab694c701
Brindha-hilife <105338847+Brindha-hilife@users.noreply.github.com>
2022-07-02 11:03:26 +0530

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,45 +1,48 @@
-const input1 = parseFloat(input1);
-const input2 = parseFloat(input2);
-
- 
-// function display(input){
-//     document.getElementById('viewcontent').value += input
-//     return input;
-// }
-function display(button){
-    var input = button.value;
+// display the values
+function display(input){
     document.getElementById("viewcontent").value += input;
 }
-    let result;
 
-    if (value == '+') {
-        result = input1 + input2;
-        document.getElementById("viewcontent").innerHTML = result;
-    }
-    else if (value == '-') {
-        result = input1 - input2;
-        document.getElementById("viewcontent").innerHTML = result;
-    }
-    else if (value == '-') {
-        result = input1 + input2;
-        document.getElementById("viewcontent").innerHTML = result;
-    }
-    else {
-        result = input1 / input2;
-        document.getElementById("viewcontent").innerHTML = result;
-    }
-    
+// Calculation
+function equal(){
 
-    function clear(){
-        // var output = 0;
-        document.getElementById("viewcontent").innerHTML = " ";
-    }
+//    let result = null ;
+//     var input1 = parseFloat(firstinput.innerHTML);
+//     var input2 = parseFloat(secondinput.innerHTML);
+//     var output = document.getElementById("viewcontent");
+   
 
+//     if (operator === '+') {
+//         result = input1 + input2;
+//         output.innerHTML = result;
+//         console.log(result);
+//     }
+//     else if (operator === '-') {
+//         result = input1 - input2;
+//         output.innerHTML = result;
+//     }
+//     else if (operator === '*') {
+//         result = input1 * input2;
+//         output.innerHTML = result;
+//     }
+//     else if (operator === '/') {
+//         result = input1 / input2;
+//         output.innerHTML = result;
+//     }else{
+//         output.innerHTML = result;
+//     }
+    result = document.getElementById("viewcontent");
+    result.value = eval(result.value);  
+}
 
-// function del(){
-//     const input = document.querySelector('input');
-//     input.addEventListener('click', function() {
-//         input.innerHTML = slice(0, -1);
-//     })
-    
-// }
+// clear the screen
+function allClear(){
+    document.getElementById("viewcontent").value="";
+}
+
+// delete the numbers from the right side
+function del(){
+    var newnumber = document.getElementById("viewcontent");
+    var delt = newnumber.value.slice(0, -1);
+    newnumber.value = delt;
+}
